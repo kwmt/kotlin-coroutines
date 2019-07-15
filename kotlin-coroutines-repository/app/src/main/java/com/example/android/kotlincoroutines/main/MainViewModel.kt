4817396731fe0a9fb2d500ab4f7dd40bafeebe06
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-07-15 13:47:44 +0900

    loadingとエラー表示のリファクタリング suspendラムダを使うことでsuspend関数を渡せるようになる

--- kotlin-coroutines-repository/app/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ kotlin-coroutines-repository/app/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -21,6 +21,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 /**
@@ -114,15 +115,8 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
 //        }
 //    }
     fun refreshTitle() {
-        viewModelScope.launch {
-            try {
-                _spinner.value = true
-                repository.refreshTitle()
-            } catch (error: TitleRefreshError) {
-                _snackBar.value = error.message
-            } finally {
-                _spinner.value = false
-            }
+        launchDataLoad {
+            repository.refreshTitle()
         }
     }
 
@@ -138,4 +132,17 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      *              spinner will stop
      */
     // TODO: Add launchDataLoad here then refactor refreshTitle to use it
+    private fun launchDataLoad(block: suspend () -> Unit) : Job {
+        return  viewModelScope.launch {
+            try {
+                _spinner.value = true
+                block()
+            } catch (error: TitleRefreshError) {
+                _snackBar.value = error.message
+            } finally {
+                _spinner.value = false
+            }
+        }
+
+    }
 }
