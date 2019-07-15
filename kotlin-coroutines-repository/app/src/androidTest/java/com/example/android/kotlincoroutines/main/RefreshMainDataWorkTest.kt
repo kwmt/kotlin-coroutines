26477d25882dec4622eca0c854404e5f6c7f6756
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-07-15 18:13:43 +0900

    androidx.test.core.app.ApplicationProviderってandroidx.test.espresso:espresso-intentsに入ってるのか！

---
+++ kotlin-coroutines-repository/app/src/androidTest/java/com/example/android/kotlincoroutines/main/RefreshMainDataWorkTest.kt
@@ -0,0 +1,19 @@
+package com.example.android.kotlincoroutines.main
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class RefreshMainDataWorkTest {
+    private lateinit var context: Context
+
+    @Before
+    fun setup() {
+        context = ApplicationProvider.getApplicationContext()
+
+        Defau
+    }
+}
